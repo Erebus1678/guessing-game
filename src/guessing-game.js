e2506d7363638d222d0e28e9fddf284bb57c5eb5
Erebus1678 <furyman1678@gmail.com>
2022-08-16 17:28:06 +0300

    feat: solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.res = Math.round((this.min + this.max) / 2);
+        return this.res;
     }
 
     lower() {
-
+        this.max = this.res;
     }
 
     greater() {
-
+        this.min = this.res;
     }
 }
 
